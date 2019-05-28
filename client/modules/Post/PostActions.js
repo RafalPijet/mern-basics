f10f584e8d79c02fc244023d7e24481d8d1ef4da
Rafal Pijet <stronglopez@wp.pl>
2019-05-28 13:16:18 +0200

    Add example from 30.5 modul

--- client/modules/Post/PostActions.js
+++ client/modules/Post/PostActions.js
@@ -4,11 +4,20 @@ import callApi from '../../util/apiCaller';
 export const ADD_POST = 'ADD_POST';
 export const ADD_POSTS = 'ADD_POSTS';
 export const DELETE_POST = 'DELETE_POST';
+export const EDIT_POST = 'EDIT_POST';
 
 // Export Actions
 export function addPost(post) {
   return {
     type: ADD_POST,
+    post,
+  };
+}
+
+export function editPost(cuid, post) {
+  return {
+    type: EDIT_POST,
+    cuid,
     post,
   };
 }
@@ -22,6 +31,18 @@ export function addPostRequest(post) {
         content: post.content,
       },
     }).then(res => dispatch(addPost(res.post)));
+  };
+}
+
+export function editPostRequest(cuid, post) {
+  return (dispatch) => {
+    return callApi(`posts/${cuid}`, 'put', {
+      post: {
+        name: post.name,
+        title: post.title,
+        content: post.content,
+      },
+    }).then(() => dispatch(editPost(cuid, post)));
   };
 }
 
